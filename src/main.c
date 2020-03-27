40041d2e938cbca560b304be098ae4f71541db0e
Geolm <moyaguillaume@gmail.com>
2020-03-27 13:45:05 -0400

    added param parsing

--- src/main.c
+++ src/main.c
@@ -7,10 +7,34 @@
 //
 
 #include <stdio.h>
-#include "rasterization.h"
+#include <stdlib.h>
+#include "parg.h"
 
 int main(int argc, const char * argv[])
 {
-    
+    struct parg_state ps;
+    int c;
+
+    if (argc == 1)
+    {
+        printf("flow [-h height] [-w width] [-o output_filename]");
+        return -1;
+    }
+
+    int output_width = 256;
+    int output_height = 256;
+    const char* output_filename = "output.png";
+
+    parg_init(&ps);
+    while ((c = parg_getopt(&ps, argc, argv, "who")) != -1) 
+    {
+		switch (c) 
+        {
+        case 'w' : output_width = atoi(ps.optarg); break;
+        case 'h' : output_height = atoi(ps.optarg); break;
+        case 'o' : output_filename = ps.optarg; break;
+        }
+    }
+
     return 0;
 }
