f47793f6b6645174c2e5d31fb1f134795c386250
Geolm <moyaguillaume@gmail.com>
2020-04-12 09:29:20 -0400

    added bucket

---
+++ src/bucket.c
@@ -0,0 +1,7 @@
+#include "bucket.h"
+
+//-----------------------------------------------------------------------------
+void init_buckets(bucket** buckets, int height, int num_buckets, int max_particles)
+{
+
+}
---
+++ src/bucket.h
@@ -0,0 +1,19 @@
+#ifndef __BUCKET__H_
+#define __BUCKET__H_
+
+#include "simulation.h"
+
+
+typedef struct
+{
+    vec2 aabb_min;
+    vec2 aabb_max;
+    int row_start;
+    int row_end;
+    int particles_count;
+    particle* particles;
+} bucket;
+
+void init_buckets(bucket** buckets, int height, int num_buckets, int max_particles);
+
+#endif
