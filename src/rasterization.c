e7baa716e9ed3757dd105f4a034ec5f76be67a47
Geolm <moyaguillaume@gmail.com>
2020-04-14 10:52:36 -0400

    refactor rasterization

--- src/rasterization.c
+++ src/rasterization.c
@@ -7,7 +7,12 @@ static inline float clamp(float value, float min_value, float max_value)
 }
 
 //-----------------------------------------------------------------------------
-float compute_pixel_opacity(vec2 top_left, vec2 offset_step, sq_distance_func_ptr sq_distance_func, void* params, float squared_distance)
+// MSAA 16x rasterization : check distance distance against disc for all 16 samples
+// 
+// top_left of the pixel (uv)
+// offset_step is 1/5 of pixel (uv)
+// squared_distance if the function returns below that the sample is considered opaque
+float compute_disc_opacity(vec2 top_left, vec2 offset_step, vec2 center, float squared_distance)
 {
     float opacity = 0.f;
     vec2 offset = offset_step;
@@ -18,7 +23,7 @@ float compute_pixel_opacity(vec2 top_left, vec2 offset_step, sq_distance_func_pt
         {            
             vec2 sample_position = vec2_add(top_left, offset);
             
-            if (sq_distance_func(sample_position, params) < squared_distance)
+            if (vec2_sq_distance(sample_position, center) < squared_distance)
             {
                 opacity += 0.0625f; // 1.f/16.f
             }            
@@ -30,20 +35,41 @@ float compute_pixel_opacity(vec2 top_left, vec2 offset_step, sq_distance_func_pt
 }
 
 //-----------------------------------------------------------------------------
-float disc_sq_distance(vec2 sample_position, void* params)
+inline static float line_sq_distance(vec2 sample_position, vec2 a, vec2 b)
 {
-    disc_rasterization_params* disc_params = (disc_rasterization_params*) params;
-    return vec2_sq_distance(disc_params->disc_center, sample_position);
-}
-
-//-----------------------------------------------------------------------------
-float line_sq_distance(vec2 sample_position, void* params)
-{
-    line_rasterization_params* line_params = (line_rasterization_params*) params;
-
-    vec2 pa = vec2_sub(sample_position, line_params->a);
-    vec2 ba = vec2_sub(line_params->b, line_params->a);
+    vec2 pa = vec2_sub(sample_position, a);
+    vec2 ba = vec2_sub(b, a);
     float h = clamp(vec2_dot(pa,ba)/vec2_dot(ba,ba), 0.f, 1.f);
     
     return vec2_sq_length(vec2_sub(pa, vec2_scale(ba, h)));
+}
+
+//-----------------------------------------------------------------------------
+// kind of copy/paste from the previous function, template? ;)
+float compute_line_opacity(vec2 top_left, vec2 offset_step, vec2 a, vec2 b, float squared_width)
+{
+    float opacity = 0.f;
+    vec2 offset = offset_step;
+    for(int y=0; y<4; ++y)  
+    {
+        offset.x = offset_step.x;
+        for(int x=0; x<4; ++x)
+        {            
+            vec2 sample_position = vec2_add(top_left, offset);
+            
+            if (line_sq_distance(sample_position, a, b) < squared_width)
+            {
+                opacity += 0.0625f; // 1.f/16.f
+            }            
+            offset.x += offset_step.x;
+        }
+        offset.y += offset_step.y;
+    }
+    return opacity;
+}
+
+//-----------------------------------------------------------------------------
+void rasterize_line(image_buffers *buffer, vec2 p0, vec2 p1, float width, int row_start, int row_end)
+{
+
 }
--- src/rasterization.h
+++ src/rasterization.h
@@ -2,31 +2,11 @@
 #define __RASTERIZATION_H__
 
 #include "extern/vec2.h"
-
-typedef float (*sq_distance_func_ptr)(vec2 sample_position, void* params);
-
-// MSAA 16x rasterization : check distance distance against primitives for all 16 samples
-// 
-// top_left of the pixel (uv)
-// offset_step is 1/5 of pixel (uv)
-// sq_distance_func function that computes the squared distance to sample position
-// squared_distance if the function returns below that the sample is considered opaque
-float compute_pixel_opacity(vec2 top_left, vec2 offset_step, sq_distance_func_ptr sq_distance_func, void* params, float squared_distance);
+#include "image_buffers.h"
 
 //-----------------------------------------------------------------------------
-typedef struct 
-{
-    vec2 disc_center;
-} disc_rasterization_params;
+void rasterize_line(image_buffers *buffer, vec2 p0, vec2 p1, float width, int row_start, int row_end);
+void rasterize_disc(image_buffers *buffer, vec2 center, float radius, int row_start, int row_end);
 
-typedef struct 
-{
-    vec2 a;
-    vec2 b;
-} line_rasterization_params;
-
-//-----------------------------------------------------------------------------
-float disc_sq_distance(vec2 sample_position, void* params);
-float line_sq_distance(vec2 sample_position, void* params);
 
 #endif // __RASTERIZATION_H__
